7b730c0884c1d24f243792863da37bf0ef664b69
xuejp <825489371@qq.com>
2018-08-14 14:11:54 +0800

    Signed-off-by: xuejp <825489371@qq.com>

---
+++ WebContent/user.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=utf-8"
+    pageEncoding="utf-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<a href="user.do">通过beanname访问Controller</a><br/>
+	<a href="userInfo.do">通过url访问Controller</a><br/>
+	<a href="userController.do">通过类名访问Controller</a><br/>
+</body>
+</html>
---
+++ src/com/xuejp/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.xuejp.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class UserController extends AbstractController {
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		System.out.println("==========∑√Œ µΩ¡À==============");
+		ModelAndView mav = new ModelAndView("user");
+		return mav;
+	}
+
+}
